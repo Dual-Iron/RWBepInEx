d4cecbc65e1bd5b7c70fe8c9f184d7025d5741e6
Dual-Iron <31146412+Dual-Iron@users.noreply.github.com>
2021-01-02 22:24:31 -0500

    Going well so far

---
+++ BepInEx.Partiality.Patcher/Patcher.cs
@@ -0,0 +1,64 @@
+﻿using BepInEx.Logging;
+using Mono.Cecil;
+using System;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+namespace BepInEx.Partiality.Patcher
+{
+    public partial class Patcher
+    {
+        private readonly ModuleDefinition module;
+        private readonly int id;
+
+        private static readonly MethodBase ignoreAccessChecksCtor = typeof(IgnoresAccessChecksToAttribute).GetConstructor(new[] { typeof(string) });
+        private static readonly ManualLogSource logger = Logger.CreateLogSource("PartPatch");
+
+        private static int globalId;
+
+        public Patcher(ModuleDefinition module)
+        {
+            id = globalId++;
+            this.module = module;
+        }
+
+        public void UpdateMonoModHookNames()
+        {
+            logger.LogInfo("${id}: Updating MonoMod HookGen names.");
+
+            try
+            {
+
+            }
+            catch (Exception e)
+            {
+                logger.LogError($"{id}: Failed updating MonoMod hook names. " + e);
+            }
+        }
+
+        public void IgnoreAccessChecks()
+        {
+            logger.LogInfo($"{id}: Adding IgnoresAccessChecksToAttribute.");
+
+            try
+            {
+                var ctorRef = module.ImportReference(ignoreAccessChecksCtor);
+                var item = new CustomAttribute(ctorRef);
+                var attributeArg = new CustomAttributeArgument(module.TypeSystem.String, "Assembly-CSharp");
+
+                item.ConstructorArguments.Add(attributeArg);
+
+                module.Assembly.CustomAttributes.Add(item);
+            }
+            catch (Exception e)
+            {
+                logger.LogError($"{id}: Failed adding attribute. " + e);
+            }
+        }
+
+        public void Finish()
+        {
+        }
+    }
+}
---
+++ BepInEx.Partiality.Patcher/PatcherEntrance.cs
@@ -0,0 +1,51 @@
+﻿using BepInEx.Logging;
+using BepInEx.Preloader.Patching;
+using Mono.Cecil;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+
+namespace BepInEx.Partiality.Patcher
+{
+    public static class PatcherEntrance
+    {
+        private static readonly string partDirectory = Directory.CreateDirectory(Paths.PluginPath + "\\..\\partiality").FullName;
+
+        static PatcherEntrance()
+        {
+            // Ensure all assemblies we want are loaded
+            AssemblyPatcher.PatchAndLoad(Paths.PluginPath);
+            AssemblyPatcher.PatchAndLoad(partDirectory);
+        }
+
+        public static IEnumerable<string> TargetDLLs
+        {
+            get
+            {
+                // Just target all DLLs in the plugin and partiality directory.
+                foreach (var item in Directory.GetFiles(partDirectory, "*.dll"))
+                {
+                    yield return Path.GetFileName(item);
+                }
+                foreach (var item in Directory.GetFiles(Paths.PluginPath, "*.dll"))
+                {
+                    yield return Path.GetFileName(item);
+                }
+            }
+        }
+
+        public static void Patch(AssemblyDefinition assembly)
+        {
+            // If it references the old HOOKS-Assembly-CSharp, fix it!
+            if (assembly.MainModule.AssemblyReferences.Any(n => n.FullName.StartsWith("HOOKS-Assembly-CSharp")))
+            {
+                var patcher = new Patcher(assembly.MainModule);
+                patcher.UpdateMonoModHookNames();
+                patcher.IgnoreAccessChecks();
+                patcher.Finish();
+            }
+        }
+    }
+}
